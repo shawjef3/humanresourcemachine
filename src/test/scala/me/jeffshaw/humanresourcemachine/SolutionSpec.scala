2e4119a87c04cffaa1a0b7cb3331ad6993be02be
Jeff Shaw <shawjef3@gmail.com>
2019-03-01 00:35:20 -0500

    make sure there is only one result add instruction numbers

--- src/test/scala/me/jeffshaw/humanresourcemachine/SolutionSpec.scala
+++ src/test/scala/me/jeffshaw/humanresourcemachine/SolutionSpec.scala
@@ -36,30 +36,29 @@ class SolutionSpec extends FunSuite {
       Solution(
         level = level,
         instructions = Vector(
-          Inbox,
-          CopyTo(0),
-          Outbox,
-          CopyFrom(0),
-          JumpIfZero(0),
-          JumpIfNegative(11),
-          BumpDown(0),
-          Outbox,
-          CopyFrom(0),
-          JumpIfZero(0),
-          Jump(3),
+    /* 0*/Inbox,
+    /* 1*/CopyTo(0),
+    /* 2*/Outbox,
+    /* 3*/CopyFrom(0),
+    /* 4*/JumpIfZero(0),
+    /* 5*/JumpIfNegative(11),
+    /* 6*/BumpDown(0),
+    /* 7*/Outbox,
+    /* 8*/CopyFrom(0),
+    /* 9*/JumpIfZero(0),
+    /*10*/Jump(3),
           // negative branch
-          CopyFrom(0),
-          BumpUp(0),
-          Outbox,
-          CopyFrom(0),
-          JumpIfZero(0),
-          Jump(11)
+    /*11*/CopyFrom(0),
+    /*12*/BumpUp(0),
+    /*13*/Outbox,
+    /*14*/CopyFrom(0),
+    /*15*/JumpIfZero(0),
+    /*16*/Jump(11)
         )
       )
 
-    val result = solution.results
-
-    assertResult(condition.expectedOut)(result(condition).fold(_ => ???, states => states.last.out))
+    assertResult(1)(solution.results)
+    assertResult(condition.expectedOut)(solution.results(condition).fold(_ => ???, states => states.last.out))
   }
 
 }
